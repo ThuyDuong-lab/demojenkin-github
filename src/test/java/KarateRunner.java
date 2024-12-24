abc5e826f97612fc7b02caf8201853f080889ac7
ThuyDuong-lab <duongphuongthuy1105@gmail.com>
2024-12-24 13:47:12 +0700

    Update KarateRunner.java

--- src/test/java/KarateRunner.java
+++ src/test/java/KarateRunner.java
@@ -1,6 +1,6 @@
 import com.intuit.karate.junit5.Karate;
 
-public class KarateRunner1 {
+public class KarateRunner {
     @Karate.Test
     Karate runTest() {
 //        String filePath = System.getProperty("karate.test");
@@ -8,7 +8,7 @@ public class KarateRunner1 {
 //            throw new IllegalArgumentException("Error");
 //        }
 //        String fileName = filePath.replace('\\', '/');
-//        return Karate.run(fileName).relativeTo(KarateRunner1.class);
-        return Karate.run("testcases/orangehrm/TC_001").relativeTo(KarateRunner1.class);
+//        return Karate.run(fileName).relativeTo(KarateRunner.class);
+        return Karate.run("testcases/orangehrm/TC_001").relativeTo(KarateRunner.class);
     }
 }
